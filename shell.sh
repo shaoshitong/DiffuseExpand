c7659e98dc221492f30810df0d0dc668ca82d625
shaoshitong <1090784053@qq.com>
2023-02-23 20:16:16 +0800

    anantmous4

--- shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python eval.py --dataset=CGMH --loss_type sigmoid_l1 --model=Unet --train_epochs=50 --generate_data_path /home/sst/product/Synthetic-Medical-Images/pytorch_Stage_I/images \
---num_experts=100 --buffer_path=/home/Bigdata/mtt_distillation_ckpt \
---data_path=/home/Bigdata/medical_dataset/CGMH_PelvisSegment \
---csv_path=/home/Bigdata/medical_dataset/COVID/covid-chestxray-dataset-master/metadata.csv
